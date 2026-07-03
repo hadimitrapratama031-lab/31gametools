94f6c2c424b798ccc990d27ffbd94f69e0b9a07a
hadimitrapratama031-lab <hadimitrapratama031@gmail.com>
2026-07-03 09:40:48 +0700

    new

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,15 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import express from 'express'
 import cors from 'cors'
-import dotenv from 'dotenv'
 import mongoose from 'mongoose'
-import authRoutes from './routes/auth'
-import gameRoutes from './routes/games'
-import userRoutes from './routes/users'
+import authRoutes    from './routes/auth'
+import gameRoutes    from './routes/games'
+import userRoutes    from './routes/users'
 import historyRoutes from './routes/history'
 import voucherRoutes from './routes/vouchers'
-import adminRoutes from './routes/admin'
+import adminRoutes   from './routes/admin'
 import webhookRoutes from './routes/webhook'
 
 dotenv.config()
